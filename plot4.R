e86bd3f0092d305abd2554fc33121a4515adddc0
Chris Kuhn <christopher_lee_kuhn@yahoo.com>
2014-06-08 00:43:17 -0500

    saved plot4.R

--- plot4.R
+++ plot4.R
@@ -34,7 +34,7 @@ plot(data_2days[ , 2],data_2days[ , 7], type = 'n', ylab = "Energy sub metering"
 lines(data_2days[ , 2],data_2days[ , 7])
 lines(data_2days[ , 2],data_2days[ , 8], col = "red")
 lines(data_2days[ , 2],data_2days[ , 9], col = "blue")
-legend("topright", lty=c(1,1), col = c("black", "red", "blue"), legend = c("Sub_metering_1",
+legend("topright", bty = 'n', lty=c(1,1), col = c("black", "red", "blue"), legend = c("Sub_metering_1",
        "Sub_metering_2", "Sub_metering_3"))
 
 ##upper right
